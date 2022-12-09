24f82e8dcec0b887f628cc6357870e2c15f16f59
DaeHyeonHong <fholng@outlook.com>
2022-12-09 16:53:08 +0900

    elvis expression

--- src/main/kotlin/lec02/Lec02Main.kt
+++ src/main/kotlin/lec02/Lec02Main.kt
@@ -1,20 +1,29 @@
 package lec02
 
 fun main() {
-
+    val str: String? = null
+    startWith("ABC")
+    startWith("BCD")
+    startWith(str )
+    println(str?.length ?: 0)
+    println(startWithA2(str))
+    println(startWithA3(str))
+    println(startWithA1(str))
 }
 
 fun startWithA1(str: String?): Boolean {
-    if (str == null) throw IllegalArgumentException("null이 들어왔습니다.")
-    return str.startsWith("A")
+    return str?.startsWith("A")
+        ?: throw IllegalArgumentException("null이 들어왔습니다.")
 }
 
 fun startWithA2(str: String?): Boolean? {
-    if (str == null) return null
-    return str.startsWith("A")
+    return str?.startsWith("A")
 }
 
 fun startWithA3(str: String?): Boolean {
-    if (str == null) return false
-    return str.startsWith("A")
+    return str?.startsWith("A") ?: false
+}
+
+fun startWith(str: String?): Boolean {
+    return str!!.startsWith("A")
 }
