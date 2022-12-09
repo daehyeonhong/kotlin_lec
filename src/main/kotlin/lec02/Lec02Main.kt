40c4535e617bdc27e9621c2000386145db1addf4
DaeHyeonHong <fholng@outlook.com>
2022-12-09 16:59:45 +0900

    자바코드 널 유의;

--- src/main/kotlin/lec02/Lec02Main.kt
+++ src/main/kotlin/lec02/Lec02Main.kt
@@ -1,29 +1,10 @@
 package lec02
 
 fun main() {
-    val str: String? = null
-    startWith("ABC")
-    startWith("BCD")
-    startWith(str )
-    println(str?.length ?: 0)
-    println(startWithA2(str))
-    println(startWithA3(str))
-    println(startWithA1(str))
+    val person: Person = Person("")
+    println(startWithA1(person.name))
 }
 
-fun startWithA1(str: String?): Boolean {
-    return str?.startsWith("A")
-        ?: throw IllegalArgumentException("null이 들어왔습니다.")
-}
-
-fun startWithA2(str: String?): Boolean? {
-    return str?.startsWith("A")
-}
-
-fun startWithA3(str: String?): Boolean {
-    return str?.startsWith("A") ?: false
-}
-
-fun startWith(str: String?): Boolean {
-    return str!!.startsWith("A")
+fun startWithA1(str: String): Boolean {
+    return str.startsWith("A")
 }
