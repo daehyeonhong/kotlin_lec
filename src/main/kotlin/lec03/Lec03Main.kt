5bd6cef1df7523cb804701dc455615b6ff6def35
DaeHyeonHong <fholng@outlook.com>
2022-12-10 20:40:28 +0900

    type

---
+++ src/main/kotlin/lec03/Lec03Main.kt
@@ -0,0 +1,11 @@
+package lec03
+
+fun main() {
+    val number1: Int = 3
+    val number2: Long = number1.toLong()
+    val number3: Int = 3
+    val number4: Int = 5
+    val result: Double = number3 / number4.toDouble()
+    println(number2)
+    println(result)
+}
