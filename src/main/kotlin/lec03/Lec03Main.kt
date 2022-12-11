ff37281bcfcdd5b25f96634a991b13702573808b
DaeHyeonHong <fholng@outlook.com>
2022-12-11 23:50:34 +0900

    type casting

--- src/main/kotlin/lec03/Lec03Main.kt
+++ src/main/kotlin/lec03/Lec03Main.kt
@@ -1,13 +1,13 @@
 package lec03
 
+
 fun main() {
     val number1: Int? = 3
     val number2: Long = number1?.toLong() ?: 0L
+    objectObjIsPerson(Person("a", 1))
 }
 
-fun objectObjIsPerson(obj: Any) {
-    if (obj !is Person) {
-//        println(obj.age)
-        println("obj is not Person")
-    }
+fun objectObjIsPerson(obj: Any?) {
+    val person = obj as? Person
+    println(person?.age)
 }
