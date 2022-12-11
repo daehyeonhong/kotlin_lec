cc18f783340aa62a44063373a009964e09d57e18
DaeHyeonHong <fholng@outlook.com>
2022-12-11 23:53:29 +0900

    cleanup ignore;

--- src/main/kotlin/lec03/Lec03Main.kt
+++ src/main/kotlin/lec03/Lec03Main.kt
@@ -4,7 +4,7 @@ package lec03
 fun main() {
     val number1: Int? = 3
     val number2: Long = number1?.toLong() ?: 0L
-    objectObjIsPerson(Person("a", 1))
+    objectObjIsPerson(Person("a", 100))
 }
 
 fun objectObjIsPerson(obj: Any?) {
