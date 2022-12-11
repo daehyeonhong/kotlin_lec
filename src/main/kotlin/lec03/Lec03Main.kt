f6434db6bca34604f3508b8a028034ccb9b9ec7d
DaeHyeonHong <fholng@outlook.com>
2022-12-11 21:25:44 +0900

    smart casting

--- src/main/kotlin/lec03/Lec03Main.kt
+++ src/main/kotlin/lec03/Lec03Main.kt
@@ -4,3 +4,9 @@ fun main() {
     val number1: Int? = 3
     val number2: Long = number1?.toLong() ?: 0L
 }
+
+fun objectObjIsPerson(obj: Any) {
+    if (obj is Person) {
+        println(obj.age)
+    }
+}
--- src/main/kotlin/lec03/Person.java
+++ src/main/kotlin/lec03/Person.java
@@ -1,17 +1,23 @@
-package lec02;
+package lec03;
 
 
 import org.jetbrains.annotations.Nullable;
 
 public class Person {
     private final String name;
+    private final int age;
 
-    public Person(final String name) {
+    public Person(final String name, final int age) {
         this.name = name;
+        this.age = age;
     }
 
     @Nullable
     public String getName() {
         return name;
+    }
+    @Nullable
+    public int getAge() {
+        return age;
     }
 }
