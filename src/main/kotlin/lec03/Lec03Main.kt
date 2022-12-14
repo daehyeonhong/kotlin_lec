df2417fb8ea21616ff7eae5a208f68cb41127faf
DaeHyeonHong <fholng@outlook.com>
2022-12-15 06:59:17 +0900

    message template

--- src/main/kotlin/lec03/Lec03Main.kt
+++ src/main/kotlin/lec03/Lec03Main.kt
@@ -5,6 +5,9 @@ fun main() {
     val number1: Int? = 3
     val number2: Long = number1?.toLong() ?: 0L
     objectObjIsPerson(Person("a", 100))
+
+    val person: Person = Person("lala", 10)
+    println("내 이름은 ${person.name} 나이는 ${person.age} 입니다.")
 }
 
 fun objectObjIsPerson(obj: Any?) {
