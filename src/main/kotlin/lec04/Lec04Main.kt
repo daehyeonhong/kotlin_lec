56c0ebe8490816e123d7491063b3eaf96128a7da
DaeHyeonHong <fholng@outlook.com>
2022-12-16 07:32:37 +0900

    logical

--- src/main/kotlin/lec04/Lec04Main.kt
+++ src/main/kotlin/lec04/Lec04Main.kt
@@ -1,14 +1,17 @@
 package lec04
 
 fun main() {
-    val money1 = JavaMoney(1_000L)
-    val money2 = money1
-    val money3 = JavaMoney(1_000L)
-    println(money1 === money2)
-    println(money1 === money3)
-    println(money2 === money3)
+    if (fun2() && fun1()) {
+        println("본문")
+    }
+}
 
-    println(money1 == money2)
-    println(money1 == money3)
-    println(money2 == money3)
+fun fun2(): Boolean {
+    println("fun2")
+    return true
+}
+
+fun fun1(): Boolean {
+    println("fun1")
+    return false
 }
