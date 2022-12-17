e639000b12e951db6e2ed2a01377e9ea4bf06705
DaeHyeonHong <fholng@outlook.com>
2022-12-17 10:20:51 +0900

    switchWithWhen

--- src/main/kotlin/Lec05/Lec05Main.kt
+++ src/main/kotlin/Lec05/Lec05Main.kt
@@ -5,7 +5,8 @@ fun main() {
     getGrade(20)
     getGradeWithSwitch(80)
     startWithA("DS")
-
+    judgeNumber(1)
+    judgeNumber2(123)
     validateScore(-1)
 }
 
@@ -43,5 +44,13 @@ fun judgeNumber(number: Int): Unit {
     when (number) {
         1, 0, -1 -> println("어디서 많이 본 숫자입니다.")
         else -> println("1,0,-1이 아닙니다.")
+    }
+}
+
+fun judgeNumber2(number: Int): Unit {
+    when {
+        number == 0 -> println("주어진 숫자는 0입니다.")
+        number % 2 == 0 -> println("주어진 숫자는 짝수입니다.")
+        else -> println("주어진 숫자는 홀수입니다.")
     }
 }
