5f3473a2ece255fe1e6a8f030ed415ad5e28d5c9
DaeHyeonHong <fholng@outlook.com>
2022-12-17 09:50:07 +0900

    elif-condition

--- src/main/kotlin/Lec05/Lec05Main.kt
+++ src/main/kotlin/Lec05/Lec05Main.kt
@@ -1,4 +1,20 @@
 package Lec05
 
-class Lec05Main {
+fun main() {
+    println(getPassOrFail(20))
+    validateScoreIsNotNegative(-1)
+}
+
+fun validateScoreIsNotNegative(score: Int): Unit {
+    if (score < 0) {
+        throw IllegalArgumentException("score는 0보다 작을 수 없습니다.")
+    }
+}
+
+fun getPassOrFail(score: Int): String {
+    return if (score >= 50) "P" else "F"
+}
+
+fun getGrade(score: Int): String {
+    return if (score >= 90) "A" else if (score >= 80) "B" else if (score >= 70) "C" else "D"
 }
