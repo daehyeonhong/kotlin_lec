798a3c8d54a50a86f0aa98f90b303b4371d602a1
DaeHyeonHong <fholng@outlook.com>
2022-12-17 09:55:59 +0900

    switch-case-with-range(in)

--- src/main/kotlin/Lec05/Lec05Main.kt
+++ src/main/kotlin/Lec05/Lec05Main.kt
@@ -21,10 +21,10 @@ fun getGrade(score: Int): String {
 }
 
 fun getGradeWithSwitch(score: Int): String {
-    return when (score / 10) {
-        9 -> "A"
-        8 -> "B"
-        7 -> "C"
+    return when (score) {
+        in 90..99 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
         else -> "D"
     }
 }
