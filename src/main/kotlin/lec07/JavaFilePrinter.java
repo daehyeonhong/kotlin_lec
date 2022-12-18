3e53478697b685a2631bebcf07480abd19c65f72
DaeHyeonHong <fholng@outlook.com>
2022-12-19 08:49:56 +0900

    try-with-resources

--- src/main/kotlin/lec07/JavaFilePrinter.java
+++ src/main/kotlin/lec07/JavaFilePrinter.java
@@ -1,16 +1,13 @@
 package lec07;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class JavaFilePrinter {
-    public void readFile() throws IOException {
-        final File currentFile = new File(".");
-        final File file = new File(currentFile.getAbsolutePath() + "/a.txt");
-        final BufferedReader reader = new BufferedReader(new FileReader(file));
-        System.out.println(reader.readLine());
-        reader.close();
+    public void readFile(final String path) throws IOException {
+        try (final BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            System.out.println(reader.readLine());
+        }
     }
 }
