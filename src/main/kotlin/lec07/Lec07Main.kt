886cceefecaff9d4b64c7e6e9453068a5d046b3c
DaeHyeonHong <fholng@outlook.com>
2022-12-19 08:37:30 +0900

    try-catch-expression

---
+++ src/main/kotlin/lec07/Lec07Main.kt
@@ -0,0 +1,21 @@
+package lec07
+
+fun main() {
+    parseIntOrThrow("125a")
+}
+
+fun parseIntOrThrow(string: String): Int {
+    try {
+        return string.toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("주어진 문자열 \"${string}\"은 숫자형이 아닙니다.")
+    }
+}
+
+fun parseIntOfThrow2(string: String): Int? {
+    return try {
+        string.toInt()
+    } catch (e: NumberFormatException) {
+        null
+    }
+}
