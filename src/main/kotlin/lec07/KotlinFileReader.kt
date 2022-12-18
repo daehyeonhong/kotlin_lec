ff14a4624097470186cfbd82259cfea28da84908
DaeHyeonHong <fholng@outlook.com>
2022-12-19 08:51:31 +0900

    use

--- src/main/kotlin/lec07/KotlinFileReader.kt
+++ src/main/kotlin/lec07/KotlinFileReader.kt
@@ -1,15 +1,12 @@
 package lec07
 
 import java.io.BufferedReader
-import java.io.File
 import java.io.FileReader
 
 class KotlinFileReader {
-    fun readFile() {
-        val currentFile = File(".")
-        val file = File(currentFile.absolutePath + "/a.txt")
-        val reader = BufferedReader(FileReader(file))
-        println(reader.readLine())
-        reader.close()
+    fun readFile(path: String) {
+        BufferedReader(FileReader(path)).use { reader ->
+            println(reader.readLine())
+        }
     }
 }
