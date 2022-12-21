95522d17bdf6ee40263c665d343c4e828c42a55e
DaeHyeonHong <fholng@outlook.com>
2022-12-21 18:06:03 +0900

    kotlin default parameter

--- src/main/kotlin/lec08/Lec08Main.kt
+++ src/main/kotlin/lec08/Lec08Main.kt
@@ -2,6 +2,11 @@ package lec08
 
 fun main() {
     println(max(1, 2))
+    repeat("Hello, World!", 3, true)
 }
 
 fun max(a: Int, b: Int) = if (a > b) a else b
+
+fun repeat(str: String, num: Int = 3, useNewLine: Boolean = true): Unit {
+    for (i in 1..num) if (useNewLine) println(str) else print(str)
+}
