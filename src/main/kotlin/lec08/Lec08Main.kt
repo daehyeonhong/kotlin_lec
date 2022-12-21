5a8ce41ab10e25d30b3a60cc4dd0458b6e2ebed2
DaeHyeonHong <fholng@outlook.com>
2022-12-21 18:10:15 +0900

    named-argument builder

--- src/main/kotlin/lec08/Lec08Main.kt
+++ src/main/kotlin/lec08/Lec08Main.kt
@@ -3,10 +3,16 @@ package lec08
 fun main() {
     println(max(1, 2))
     repeat("Hello, World!", useNewLine = false)
+    printNameAndGender(name = "방구", gender = "뿡")
 }
 
 fun max(a: Int, b: Int) = if (a > b) a else b
 
 fun repeat(str: String, num: Int = 3, useNewLine: Boolean = true): Unit {
     for (i in 1..num) if (useNewLine) println(str) else print(str)
+}
+
+fun printNameAndGender(name: String, gender: String): Unit {
+    println(name)
+    println(gender)
 }
