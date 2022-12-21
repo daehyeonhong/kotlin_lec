57a919cd8c00f8ee0f445589ea93f511a4a1a371
DaeHyeonHong <fholng@outlook.com>
2022-12-21 18:13:35 +0900

    variable argument and spread exp;

--- src/main/kotlin/lec08/Lec08Main.kt
+++ src/main/kotlin/lec08/Lec08Main.kt
@@ -4,6 +4,8 @@ fun main() {
     println(max(1, 2))
     repeat("Hello, World!", useNewLine = false)
     printNameAndGender(name = "방구", gender = "뿡")
+    val array: Array<String> = arrayOf("A", "B", "C")
+    printAll(*array)
 }
 
 fun max(a: Int, b: Int) = if (a > b) a else b
@@ -15,4 +17,10 @@ fun repeat(str: String, num: Int = 3, useNewLine: Boolean = true): Unit {
 fun printNameAndGender(name: String, gender: String): Unit {
     println(name)
     println(gender)
+}
+
+fun printAll(vararg strings: String): Unit {
+    for (string in strings) {
+        println(string)
+    }
 }
