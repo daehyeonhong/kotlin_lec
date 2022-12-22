ff980ab20350c71065a6499e6c9679432dfc0e61
DaeHyeonHong <fholng@outlook.com>
2022-12-22 20:10:56 +0900

    init block

--- src/main/kotlin/lec09/Person.kt
+++ src/main/kotlin/lec09/Person.kt
@@ -1,6 +1,19 @@
 package lec09
 
+fun main() {
+    val person = Person("qwe", 123)
+    println(person.name + ", " + person.age)
+    person.age = 20
+    println(person.name + ", " + person.age)
+}
+
 class Person constructor(
     val name: String,
     var age: Int
-)
+) {
+    init {
+        if (age <= 0) {
+            throw IllegalArgumentException("나이는 ${age} 일 수 없습니다.")
+        }
+    }
+}
