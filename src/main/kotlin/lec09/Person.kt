589db6d82e94ebc3ad6885431a030b435ae69498
DaeHyeonHong <fholng@outlook.com>
2022-12-23 13:35:17 +0900

    custom getter

--- src/main/kotlin/lec09/Person.kt
+++ src/main/kotlin/lec09/Person.kt
@@ -1,13 +1,21 @@
 package lec09
 
 fun main() {
-    val person = Person("qwe", 123)
-    println(person.name + ", " + person.age)
-    person.age = 20
-    println(person.name + ", " + person.age)
+//    val person1 = Person("qwe1", 123)
+//    println(person1.name + ", " + person1.age)
+//    person1.age = 20
+//    println(person1.name + ", " + person1.age)
+//    val person2 = Person("qwe2")
+//    println(person2.name + ", " + person2.age)
+//    person2.age = 20
+//    println(person2.name + ", " + person2.age)
+    val person3 = Person()
+    println(person3.name + ", " + person3.age)
+    person3.age = 20
+    println(person3.name + ", " + person3.age)
 }
 
-class Person constructor(
+class Person constructor(// primary constructor
     val name: String,
     var age: Int
 ) {
@@ -15,5 +23,16 @@ class Person constructor(
         if (age <= 0) {
             throw IllegalArgumentException("나이는 ${age} 일 수 없습니다.")
         }
+        println("초기화 블록")
+    }
+
+    constructor(name: String) : this(name, 1)// secondary constructor
+    {
+        println("첫번째 부생성자")
+    }
+
+    constructor() : this("홍길동")// secondary constructor
+    {
+        println("두번째 부생성자")
     }
 }
