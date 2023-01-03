ec64577dec75fd964ec3957ff4f36c7e3d01fbcb
HongDaeHyeon <fholng@outlook.com>
2023-01-04 01:38:00 +0900

    map

--- src/main/kotlin/lec16/Lec16Main.kt
+++ src/main/kotlin/lec16/Lec16Main.kt
@@ -10,6 +10,22 @@ fun main() {
     }
     val mutableList: MutableList<Int> = mutableListOf(100, 200)
     mutableList.add(103)
+
+    val mutableSet: MutableSet<Long> = mutableSetOf(100L)
+
+    val oldMap: MutableMap<Int, String> = mutableMapOf()
+    oldMap[1] = "MONDAY"
+    oldMap.put(2, "TUESDAY")
+    val newMap: Map<Int, String> = mapOf(1 to "MONDAY", 2 to "TUESDAY")
+    for (key in oldMap.keys) {
+        println(key)
+        println(oldMap[key])
+    }
+
+    for ((key, value) in oldMap.entries) {
+        println(key)
+        println(value)
+    }
 }
 
 private fun printNumbers(numbers: List<Int>): Unit {
