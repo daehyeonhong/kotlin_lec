859d412e797a011ffc8d63da80a241ea0c569874
HongDaeHyeon <fholng@outlook.com>
2023-01-04 01:33:06 +0900

    list

--- src/main/kotlin/lec16/Lec16Main.kt
+++ src/main/kotlin/lec16/Lec16Main.kt
@@ -4,6 +4,12 @@ fun main() {
     val list: List<Int> = listOf(100, 200)
     val eList: List<Int> = emptyList<Int>()
     printNumbers(emptyList())
+    println(list[0])
+    for (i in list) {
+        println(i)
+    }
+    val mutableList: MutableList<Int> = mutableListOf(100, 200)
+    mutableList.add(103)
 }
 
 private fun printNumbers(numbers: List<Int>): Unit {
