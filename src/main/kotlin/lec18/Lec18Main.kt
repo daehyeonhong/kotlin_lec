c4e55ba727cf9ab4853e64086ac714b775e3aeaa
DaeHyeonHong <fholng@outlook.com>
2023-01-04 21:03:45 +0900

    kotlin_func

---
+++ src/main/kotlin/lec18/Lec18Main.kt
@@ -0,0 +1,36 @@
+package lec18
+
+
+fun main() {
+    val fruits = listOf(
+        Fruit("사과", 1_000),
+        Fruit("사과", 1_200),
+        Fruit("사과", 1_200),
+        Fruit("사과", 1_500),
+        Fruit("바나나", 3_000),
+        Fruit("바나나", 3_200),
+        Fruit("바나나", 2_500),
+        Fruit("수박", 10_000)
+    )
+    val isApple: (Fruit) -> Boolean = fun(fruit: Fruit): Boolean {
+        return fruit.name == "사과"
+    }
+    val isApple2: (Fruit) -> Boolean = { fruit: Fruit -> fruit.name == "사과" }
+
+    println(filterFruits(fruits, isApple))
+    println(filterFruits(fruits, isApple2))
+}
+
+private fun filterFruits(
+    fruits: List<Fruit>, filter: (Fruit) -> Boolean
+): List<Fruit> {
+    val results = mutableListOf<Fruit>()
+    for (fruit in fruits) if (filter(fruit)) results.add(fruit)
+    return results
+}
+
+class Fruit(
+    val name: String,
+    val price: Int
+) {
+}
