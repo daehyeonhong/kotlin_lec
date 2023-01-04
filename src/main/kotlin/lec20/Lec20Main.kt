64630f49396d4cc4a505826172e3edc0294fa49a
DaeHyeonHong <fholng@outlook.com>
2023-01-04 21:36:50 +0900

    component

--- src/main/kotlin/lec20/Lec20Main.kt
+++ src/main/kotlin/lec20/Lec20Main.kt
@@ -6,4 +6,14 @@ import lec19.Fruit as Fruit19
 fun main() {
     println(Fruit18("Orange", 2_000))
     println(Fruit19(1L, "Kiwi", 2_300, 4_300))
+    val person: Person = Person("감자", 200)
+//    val (name, age) = person
+    val name = person.component1()
+    val age = person.component2()
+    println(name + age)
 }
+
+data class Person(
+    val name: String,
+    val age: Int
+)
