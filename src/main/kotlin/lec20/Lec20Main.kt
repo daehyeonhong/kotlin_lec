41293c8b64572c3470a7b12fad5a827acb7fffdf
DaeHyeonHong <fholng@outlook.com>
2023-01-04 21:27:07 +0900

    as import

---
+++ src/main/kotlin/lec20/Lec20Main.kt
@@ -0,0 +1,9 @@
+package lec20
+
+import lec18.Fruit as Fruit18
+import lec19.Fruit as Fruit19
+
+fun main() {
+    println(Fruit18("Orange", 2_000))
+    println(Fruit19(1L, "Kiwi", 2_300, 4_300))
+}
