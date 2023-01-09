4730ec3de00bc01faec0b96a23e98e75f7dc477b
HongDaeHyeon <fholng@outlook.com>
2023-01-09 23:52:39 +0900

    elvis 연산자 및 safety call

--- src/main/kotlin/lec02_2/Lec02Main.kt
+++ src/main/kotlin/lec02_2/Lec02Main.kt
@@ -15,8 +15,7 @@ fun main() {
 }
 
 fun startWithA1(str: String?): Boolean {
-    if (str == null) throw IllegalArgumentException("null in")
-    return str.startsWith("A")
+    return str?.startsWith("A") ?: throw IllegalArgumentException("null in")
 }
 
 fun startWithA2(str: String?): Boolean? {
